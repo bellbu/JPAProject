0c71ce41b5c9f7adf3d5a45f232df662b265a652
BELLBU\bellb <bellbu@naver.com>
2026-09-23 23:36:06 +0900

    fixed untracked files

---
+++ src/main/java/com/springbook/biz/board/Board.java
@@ -0,0 +1,26 @@
+package com.springbook.biz.board;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "BOARD")
+public class Board {
+	@Id
+	@GeneratedValue
+	private int seq;
+	private String title;
+	private String writer;
+	private String content;
+	@Temporal(TemporalType.DATE)
+	private Date regDate = new Date();
+	private int cnt;
+	
+	
+	
+
+}
