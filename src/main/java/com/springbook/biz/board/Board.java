0b94aff2e6bd914d7b4a0a1d2aa6c5b8701a230e
BELLBU\bellb <bellbu@naver.com>
2026-09-24 01:37:48 +0900

    feat JPA 환경설정

--- src/main/java/com/springbook/biz/board/Board.java
+++ src/main/java/com/springbook/biz/board/Board.java
@@ -1,5 +1,7 @@
 package com.springbook.biz.board;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -19,8 +21,46 @@ public class Board {
 	@Temporal(TemporalType.DATE)
 	private Date regDate = new Date();
 	private int cnt;
-	
-	
-	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	@Override
+	public String toString() {
+		return "Board [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
+				+ regDate + ", cnt=" + cnt + "]";
+	}
 
 }
